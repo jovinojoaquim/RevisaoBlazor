2b8cc7694cfd721fa4c666a39f28de345a6d166d
Jovino Joaquim <jovinojoaquim@hotmail.com>
2025-12-10 21:05:13 -0300

    Salvar alterações do BlazingPizza

--- BlazingPizzaSolution/BlazingPizza.Model/Core/Address.cs
+++ BlazingPizzaSolution/BlazingPizza.Model/Core/Address.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazingPizza.Model.Core;
 
@@ -6,15 +7,21 @@ public class Address
 {
     public int Id { get; set; }
 
+    [Required, MinLength(3), MaxLength(100)]
     public string Name { get; set; } = "";
 
+    [Required, MinLength(5), MaxLength(100)]
     public string Line1 { get; set; }= "";
 
+    [MaxLength(100)]
     public string Line2 { get; set; } = "";
 
+    [Required, MinLength(3), MaxLength(50)]
     public string City { get; set; } = "";
 
+    [Required, MinLength(3), MaxLength(20)]
     public string Region { get; set; } = "";
 
+    [Required, RegularExpression(@"^([0-9]{5})$")]
     public string PostalCode { get; set; } = "";
 }
