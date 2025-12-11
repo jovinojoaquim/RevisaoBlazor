ffa8c3b3273bca8a1d194a519e0814f97156f2ba
Jovino Joaquim <jovinojoaquim@hotmail.com>
2025-12-10 21:34:50 -0300

    Importar BlazingPizza para repositório principal (remover gitlink)

---
+++ BlazingPizzaSolution/BlazingPizza/Controllers/SpecialsController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using BlazingPizza.Data;
+using BlazingPizza.Model.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers;
+
+[Route("specials")]
+[ApiController]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+    public SpecialsController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+    }
+
+}
